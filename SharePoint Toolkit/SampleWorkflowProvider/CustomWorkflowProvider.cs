f37f7c3b0de433101a38b5d69882ae4e742a681e
BGrego <brett@recordlion.com>
2016-06-10 11:27:13 -0500

    Added SharePoint Toolkit

---
+++ SharePoint Toolkit/SampleWorkflowProvider/CustomWorkflowProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Microsoft.SharePoint;
+using RecordLion.RecordsManager.SharePoint.Workflow;
+
+
+namespace SampleWorkflowProvider
+{
+    public class CustomWorkflowProvider : WorkflowProvider
+    {
+        private const string PROVIDER_NAME = "SampleWorkflowProvider.CustomWorkflowProvider";
+
+        public override string DisplayName
+        {
+            get { return "Custom Workflow Provider"; }
+        }
+
+        public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
+        {
+            base.Initialize(CustomWorkflowProvider.PROVIDER_NAME, config);
+        }
+
+        public override bool StartWorkflow(WorkflowContext context)
+        {
+            //Do anything here...start a third-party workflow, call a web service, modify SharePoint content, etc.
+             
+            //This just does something for demonstration purposes...
+            context.ListItem[SPBuiltInFieldId.Title] = "Provider Updated";
+            context.ListItem.Update();
+
+            return true;
+        }
+    }
+}
---
+++ SharePoint Toolkit/SampleWorkflowProvider/readme.txt
@@ -0,0 +1,12 @@
+﻿
+In order to deploy the custom workflow provider, perform the following steps:
+
+1. Copy the compiled assembly to the following location on each SharePoint Web Front End:
+
+	SharePoint 2013
+	C:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\TEMPLATE\LAYOUTS\RecordsManagement\Providers\Workflow
+
+	SharePoint 2010
+	C:\Program Files\Common Files\microsoft shared\Web Server Extensions\14\TEMPLATE\LAYOUTS\RecordsManagement\Providers\Workflow
+
+2. IIS Reset
